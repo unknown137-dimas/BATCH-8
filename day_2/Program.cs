1b51895d436cc37029614af302fdbe1817b9d2a4
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-12-19 14:23:42 +0700

    Simplified instantiation

--- day_2/Program.cs
+++ day_2/Program.cs
@@ -7,7 +7,7 @@ class Program
 	public static void Main()
 	
 	{
-		Fish bob = new Fish("Bobby", 1, "Red", "Koi");
+		Fish bob = new("Bobby", 1, "Red", "Koi");
 		Console.WriteLine(bob.SayHi());
 		bob.Swim();
 	}
