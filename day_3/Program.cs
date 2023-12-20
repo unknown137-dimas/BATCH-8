bb9c4f8adcbcfde4f5884d121012dcee6235b4a4
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-12-20 13:43:31 +0700

    Constructor overload

--- day_3/Program.cs
+++ day_3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program
+
+{
+	public static void Main()
+
+	{
+		Fish bob = new("Bobby", 1, "Red", "Koi");
+		bob.Swim();
+		Console.WriteLine(bob.SayHi());
+		Console.WriteLine(bob.SayType());
+		Console.WriteLine(bob.RepeatTheWords("I", "Love", "U"));
+		Console.WriteLine(bob.Eat("Steak"));
+	}
+}
