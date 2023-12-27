a4d8f6468d96aaa335dc4c438e1ddafcd82a388d
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-12-27 14:25:54 +0700

    Enum & Single responsibility

---
+++ day_6/CalendarEnum.cs
@@ -0,0 +1,27 @@
+public enum DaysOfWeek
+{
+	Monday,
+	Tuesday,
+	Wednesday,
+	Thursday,
+	Friday,
+	Saturday,
+	Sunday
+}
+
+public enum Month
+{
+	January,
+	February,
+	March,
+	April,
+	May,
+	June,
+	July,
+	August,
+	September,
+	October,
+	November,
+	December
+	
+}
