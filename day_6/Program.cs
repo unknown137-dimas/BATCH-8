c518a0bf402d5880555b44081bf2f6b9efabe436
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-12-27 15:07:03 +0700

    Collections

--- day_6/Program.cs
+++ day_6/Program.cs
@@ -21,6 +21,13 @@
 		int year = dob.Item4;
 		Console.WriteLine($"Nama       : {human1.GetName()}");
 		Console.WriteLine($"Lahir pada : {day}, {date} {month} {year}");
+		
+		// Collections
+		Console.WriteLine();
+		List<int> yearList = new(); // <T> to maintain typesafety
+		yearList.AddRange(new[]{2000, 2001, 2002});
+		yearList.AddRange(new[]{2003, 2004, 2020});
+		yearList.ForEach(Console.WriteLine);
 	}
 }
 
