80921b6e87c3d5c3aea5e5deb5f3dbf7feca58bb
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-12-28 16:57:16 +0700

    Fix the order

--- day_7/Program.cs
+++ day_7/Program.cs
@@ -3,12 +3,12 @@
 {
 	static void Main()
 	{
-		// Delegate
-		Console.WriteLine("\nDelegate");
-		Youtuber youtuber1 = new("Mr. Beast");
 		Subscriber sub1 = new("Dimas");
 		Subscriber sub2 = new("Fitrio");
 		
+		// Delegate
+		Console.WriteLine("\nDelegate");
+		Youtuber youtuber1 = new("Mr. Beast");
 		youtuber1.AddSubscriber(sub1.GetNotification);
 		youtuber1.AddSubscriber(sub2.GetNotification);
 		youtuber1.UploadVideo("Free Money");
