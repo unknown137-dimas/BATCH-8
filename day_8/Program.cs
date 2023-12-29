32c46f94c2415622b30502c9176ed24b0226f8d3
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2023-12-29 14:31:51 +0700

    Exception handling

---
+++ day_8/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+
+{
+	static void Main()
+	
+	{
+		// Exception Handling
+		Console.WriteLine("Exception Handling");
+		string input = "1a4"; // try input with not a number
+		int result = 0;
+		// Bad code (not validate input)
+		// Try Catch is bad, try to avoid it as much as possible, because try catch doesn't prevent exception.
+		// it's only prevent the program from crashing.
+		try
+		{
+			result = int.Parse(input);
+		}
+		catch(Exception e)
+		{
+			Console.WriteLine($"Error occured : {e.Message}");
+		}
+		finally
+		{
+			Console.WriteLine($"Try Catch : {result}");
+		}
+		
+		
+		// OK Code
+		if(int.TryParse(input, out result)) Console.WriteLine($"Validation : {result}"); // Using builtin validation method
+		else Console.WriteLine("Validation input failed");
+	}
+}
