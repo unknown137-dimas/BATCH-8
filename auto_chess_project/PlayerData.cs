4527ae0b9834e33bd13635ecb5208a74d50f6d80
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2024-01-15 19:42:26 +0700

    Add constructor and player data metric

--- auto_chess_project/PlayerData.cs
+++ auto_chess_project/PlayerData.cs
@@ -1,6 +1,12 @@
-
 class PlayerData
 {
     public List<IPiece> PlayerPieces = new();
-    public bool Winner = false;
+    public bool Winner;
+    public int Hp;
+    public int Win;
+
+    public PlayerData(int hp)
+    {
+        Hp = hp;
+    }
 }
