bbb575ce862d7f30d47e981dde8b74be9bbe26d7
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2024-01-17 13:25:38 +0700

    Move hero database to library

--- auto_chess_project/GameController.cs
+++ auto_chess_project/GameController.cs
@@ -1,128 +1,138 @@
 class GameController
 {
-    private readonly IBoard _board;
-    private readonly Dictionary<PieceTypes, int> _heroSlot = new() {{PieceTypes.Warrior, 3}, {PieceTypes.Hunter, 3}, {PieceTypes.Knight, 3}};
-    private Dictionary<IPlayer, PlayerData> _players = new();
-    public int PlayerHp {get; set;} = 10;
-    public Status CurrentGameStatus {get; set;} = Status.NotInitialized;
-    public Phases CurrentGamePhase {get; set;} = Phases.NotInitialized;
+	private readonly IBoard _board;
+	public Dictionary<string, HeroDetails> HeroesDatabase {get; private set;} = new();
+	private readonly Dictionary<PieceTypes, int> _heroSlot = new() {{PieceTypes.Warrior, 3}, {PieceTypes.Hunter, 3}, {PieceTypes.Knight, 3}};
+	private Dictionary<IPlayer, PlayerData> _players = new();
+	public int PlayerHp {get; set;} = 10;
+	public Status CurrentGameStatus {get; set;} = Status.NotInitialized;
+	public Phases CurrentGamePhase {get; set;} = Phases.NotInitialized;
 
-    public GameController(IBoard board)
-    {
-        CurrentGameStatus = Status.Initialized;
-        _board = board;
-    }
+	public GameController(IBoard board)
+	{
+		CurrentGameStatus = Status.Initialized;
+		_board = board;
+	}
 
-    public GameController(IBoard board, Dictionary<PieceTypes, int> heroSlot)
-    {
-        CurrentGameStatus = Status.Initialized;
-        _board = board;
-        _heroSlot = heroSlot;
-    }
+	public GameController(IBoard board, Dictionary<PieceTypes, int> heroSlot)
+	{
+		CurrentGameStatus = Status.Initialized;
+		_board = board;
+		_heroSlot = heroSlot;
+	}
 
-    public GameController(IBoard board, List<IPlayer> players)
-    {
-        CurrentGameStatus = Status.Initialized;
-        _board = board;
-        AddPlayer(players);
-    }
+	public GameController(IBoard board, List<IPlayer> players)
+	{
+		CurrentGameStatus = Status.Initialized;
+		_board = board;
+		AddPlayer(players);
+	}
 
-    public GameController(IBoard board, List<IPlayer> players, Dictionary<PieceTypes, int> heroSlot)
-    {
-        CurrentGameStatus = Status.Initialized;
-        _board = board;
-        _heroSlot = heroSlot;
-        AddPlayer(players);
-    }
+	public GameController(IBoard board, List<IPlayer> players, Dictionary<PieceTypes, int> heroSlot)
+	{
+		CurrentGameStatus = Status.Initialized;
+		_board = board;
+		_heroSlot = heroSlot;
+		AddPlayer(players);
+	}
 
-    // Get current game information
-    public IEnumerable<IPlayer> GetPlayers() => _players.Keys;
+	// Get current game information
+	public IEnumerable<IPlayer> GetPlayers() => _players.Keys;
 
-    public int[] GetBoardSize() => [_board.Width, _board.Height];
+	public int[] GetBoardSize() => [_board.Width, _board.Height];
+	
+	// Manage hero database
+	public bool AddHero(string heroName, HeroDetails heroDetails) => HeroesDatabase.TryAdd(heroName, heroDetails);
+	
+	public bool RemoveHero(string heroName)  => HeroesDatabase.Remove(heroName);
+	
+	public IEnumerable<string> GetHeroName() => HeroesDatabase.Keys.ToList();
+	
+	public HeroDetails GetHeroDetails(string heroName) => HeroesDatabase[heroName];
 
-    // Manage player
-    public bool AddPlayer(IPlayer newPlayer) => _players.TryAdd(newPlayer, new PlayerData(PlayerHp));
+	// Manage player
+	public bool AddPlayer(IPlayer newPlayer) => _players.TryAdd(newPlayer, new PlayerData(PlayerHp));
 
-    public void AddPlayer(IEnumerable<IPlayer> newPlayers)
-    {
-        foreach(var player in newPlayers)
-        {
-            AddPlayer(player);
-        }
-    }
+	public void AddPlayer(IEnumerable<IPlayer> newPlayers)
+	{
+		foreach(var player in newPlayers)
+		{
+			AddPlayer(player);
+		}
+	}
 
-    public bool RemovePlayer(IPlayer player) => _players.Remove(player);
+	public bool RemovePlayer(IPlayer player) => _players.Remove(player);
 
-    public PlayerData GetPlayerData(IPlayer player) => _players[player];
+	public PlayerData GetPlayerData(IPlayer player) => _players[player];
 
-    // Manage player's piece
-    public IEnumerable<Hero> GetPlayerPieces(IPlayer player) => _players[player].PlayerPieces;
-    public void AddPlayerPiece(IPlayer player, Hero piece) => _players[player].PlayerPieces.Add(piece);
+	// Manage player's piece
+	public IEnumerable<Hero> GetPlayerPieces(IPlayer player) => _players[player].PlayerPieces;
+	public void AddPlayerPiece(IPlayer player, Hero piece) => _players[player].PlayerPieces.Add(piece);
 
-    public void AddPlayerPiece(IPlayer player, IEnumerable<Hero> pieces) => _players[player].PlayerPieces.AddRange(pieces);
+	public void AddPlayerPiece(IPlayer player, IEnumerable<Hero> pieces) => _players[player].PlayerPieces.AddRange(pieces);
 
-    public bool RemovePlayerPiece(IPlayer player, Hero piece) => _players[player].PlayerPieces.Remove(piece);
+	public bool RemovePlayerPiece(IPlayer player, Hero piece) => _players[player].PlayerPieces.Remove(piece);
 
-    public bool PutPlayerPiece(Hero piece, Position position)
-    {
-        if(_board.IsPositionEmpty(position))
-        {
-            if(_board.AddHeroPosition(piece, position))
-            {
-                piece.Move(position);
-                return true;
-            }
-            return false;
-        }
-        return false;
-    }
+	public bool PutPlayerPiece(Hero piece, Position position)
+	{
+		if(_board.IsPositionEmpty(position))
+		{
+			if(_board.AddHeroPosition(piece, position))
+			{
+				piece.Move(position);
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}
 
-    public bool IsFinishedPutAllPieces(IPlayer player)
-    {
-        List<bool> piecePosition = new();
-        foreach(var piece in GetPlayerPieces(player))
-        {
-            piecePosition.Add(Array.IndexOf(piece.GetPosition(), -1) >= 0 ? false : true);
-        }
-        return !piecePosition.Contains(false);
-    }
+	public bool IsFinishedPutAllPieces(IPlayer player)
+	{
+		List<bool> piecePosition = new();
+		foreach(var piece in GetPlayerPieces(player))
+		{
+			piecePosition.Add(Array.IndexOf(piece.GetPosition(), -1) >= 0 ? false : true);
+		}
+		return !piecePosition.Contains(false);
+	}
 
-    public bool IsValidPosition(Hero piece, Hero otherPiece) => piece.HeroPosition.X != otherPiece.HeroPosition.X || piece.HeroPosition.Y != otherPiece.HeroPosition.Y;
+	public bool IsValidPosition(Hero piece, Hero otherPiece) => piece.HeroPosition.X != otherPiece.HeroPosition.X || piece.HeroPosition.Y != otherPiece.HeroPosition.Y;
 
-    public void NextPhase()
-    {
-        throw new NotImplementedException();
-    }
+	public void NextPhase()
+	{
+		throw new NotImplementedException();
+	}
 
-    public bool CheckTargetAcquitition(IPlayer player, Hero piece, IPosition position)
-    {
-        throw new NotImplementedException();
-    }
+	public bool CheckTargetAcquitition(IPlayer player, Hero piece, IPosition position)
+	{
+		throw new NotImplementedException();
+	}
 
-    public IEnumerable<IPosition> GetAllPossibleMove(IPlayer player, Hero piece)
-    {
-        throw new NotImplementedException();
-    }
-    
-    public int Attack(IPlayer player, Hero piece, IPlayer enemyPlayer, Hero enemyPiece)
-    {
-        throw new NotImplementedException();
-    }
-    
-    // Set current round winner
-    public bool SetWinner(IPlayer player) => _players[player].Winner = true;
+	public IEnumerable<IPosition> GetAllPossibleMove(IPlayer player, Hero piece)
+	{
+		throw new NotImplementedException();
+	}
+	
+	public int Attack(IPlayer player, Hero piece, IPlayer enemyPlayer, Hero enemyPiece)
+	{
+		throw new NotImplementedException();
+	}
+	
+	// Set current round winner
+	public bool SetWinner(IPlayer player) => _players[player].Winner = true;
 
-    // Generate random options
-    public IEnumerable<T> GenerateRandomHeroList<T>(in List<T> source)
-    {
-        Random random = new();
-        List<T> options = new();
-        int n = 5;
-        while(n > 0)
-        {
-            options.Add(source[random.Next(0, source.Count)]);
-            n--;
-        }
-        return options;
-    }
+	// Generate random options
+	public IEnumerable<T> GenerateRandomHeroList<T>(in List<T> source)
+	{
+		Random random = new();
+		List<T> options = new();
+		int n = 5;
+		while(n > 0)
+		{
+			options.Add(source[random.Next(0, source.Count)]);
+			n--;
+		}
+		return options;
+	}
 }
--- auto_chess_project/Program.cs
+++ auto_chess_project/Program.cs
@@ -2,243 +2,240 @@
 using Spectre.Console.Rendering;
 class Program
 {
-    static void FigletTitle(FigletFont font, string text)
-    {
-        AnsiConsole.Clear();
-        AnsiConsole.Write(
-            new FigletText(font, text)
-            .LeftJustified()
-            .Color(Color.Red)
-        );
-    }
+	static void FigletTitle(FigletFont font, string text)
+	{
+		AnsiConsole.Clear();
+		AnsiConsole.Write(
+			new FigletText(font, text)
+			.LeftJustified()
+			.Color(Color.Red)
+		);
+	}
 
-    static void Main()
-    {
-        // GAME CONFIGURATION
-        const int boardSize = 8;
-        int roll = 3;
-        var font = FigletFont.Load("../../../defaultFont.flf");
+	static void Main()
+	{
+		// GAME CONFIGURATION
+		const int boardSize = 8;
+		int roll = 3;
+		var font = FigletFont.Load("../../../defaultFont.flf");
 
-        // HERO ICONS
-        Dictionary<PieceTypes, string> heroIcons = new()
-        {
-            {PieceTypes.Knight, "🛡️"},
-            {PieceTypes.Warlock, "🌕"},
-            {PieceTypes.Warrior, "⚔️"},
-        };
+		// HERO ICONS
+		Dictionary<PieceTypes, string> heroIcons = new()
+		{
+			{PieceTypes.Knight, "🛡️"},
+			{PieceTypes.Warlock, "🌕"},
+			{PieceTypes.Warrior, "⚔️"},
+		};
 
-        // BOARD
-        var panel = new Panel(
-            new Markup(heroIcons[PieceTypes.Knight])
-        );
-        panel.Width = 4;
-        panel.Height = 3;
-        panel.Padding = new Padding(0,0,0,0);
-        var column = new Columns(panel, panel, panel);
-        column.Expand = false;
-        column.Padding = new Padding(0,0,0,0);
-        var board = new Rows(column, column, column);
-        
-        
-        // MAIN MENU
-        FigletTitle(font, "AutoChess");
-        AnsiConsole.Write(board);
-        var mainMenu = AnsiConsole.Prompt(
-            new SelectionPrompt<string>()
-            .PageSize(5)
-            .AddChoices(
-                [
-                    "Start",
-                    "Exit"
-                ]
-            )
-        );
+		// BOARD
+		var panel = new Panel(
+			new Markup(heroIcons[PieceTypes.Knight])
+		);
+		panel.Width = 4;
+		panel.Height = 3;
+		panel.Padding = new Padding(0,0,0,0);
+		var column = new Columns(panel, panel, panel);
+		column.Expand = false;
+		column.Padding = new Padding(0,0,0,0);
+		var board = new Rows(column, column, column);
+		
+		
+		// MAIN MENU
+		FigletTitle(font, "AutoChess");
+		AnsiConsole.Write(board);
+		var mainMenu = AnsiConsole.Prompt(
+			new SelectionPrompt<string>()
+			.PageSize(5)
+			.AddChoices(
+				[
+					"Start",
+					"Exit"
+				]
+			)
+		);
 
-        if(mainMenu == "Start")
-        {
-            // PLAYER NAME INPUT MENU
-            var playerName = AnsiConsole.Ask<string>("What's your [green]name[/]?");
+		if(mainMenu == "Start")
+		{
+			// PLAYER NAME INPUT MENU
+			var playerName = AnsiConsole.Ask<string>("What's your [green]name[/]?");
 
-            // GAME CONTROLLER INIT
-            var autoChess = new GameController(new Board(boardSize));
-            var player = new Player(playerName);
-            var bot = new Player("BOT");
-            autoChess.AddPlayer(player);
-            autoChess.AddPlayer(bot);
-            
-            // PICK HERO MENU
-            // TODO
-            // 1. How to edit/remove item from user (if user want to change item that already playerHeroes)
-            // 2. Fix BarChart
-            // 3. Move heroDatabase to library?
-            Dictionary<string, HeroDetails> heroesDatabase = new()
-            {
-                {"Hell Knight", new HeroDetails(PieceTypes.Knight, 700, 75, 5, 1)},
-                {"Poisonous Worm", new HeroDetails(PieceTypes.Warlock, 600, 55, 0, 3)},
-                {"God of Thunder", new HeroDetails(PieceTypes.Mage, 950, 60, 0, 3)},
-                {"Swordman", new HeroDetails(PieceTypes.Warrior, 600, 67.5, 5, 2)},
-                {"Egersis Ranger", new HeroDetails(PieceTypes.Hunter, 450, 45, 5, 5)},
-                {"Shadowcrawler", new HeroDetails(PieceTypes.Assassin, 550, 85, 5, 2)},
-                {"Storm Shaman", new HeroDetails(PieceTypes.Shaman, 800, 47.5, 5, 4)},
-                {"Warpwood Sage", new HeroDetails(PieceTypes.Druid, 650, 75, 5, 2)},
-                {"Fallen Witcher", new HeroDetails(PieceTypes.Witcher, 750, 70, 5, 1)},
-                {"Heaven Bomber ", new HeroDetails(PieceTypes.Mech, 500, 45, 5, 4)},
-                {"Goddess of Light", new HeroDetails(PieceTypes.Priest, 400, 52.5, 0, 4)},
-                {"Grand Herald", new HeroDetails(PieceTypes.Wizard, 600, 55, 0, 4)},
-            };
-            var heroesOptions = new List<string>();
-            foreach(var key in heroesDatabase.Keys)
-            {
-                heroesOptions.Add(key.ToString());
-            }
-            List<Hero> playerHeroes = (List<Hero>)autoChess.GetPlayerPieces(player);
-            var heroOptionsStat = new List<IRenderable>();
+			// GAME CONTROLLER INIT
+			var autoChess = new GameController(new Board(boardSize));
+			var player = new Player(playerName);
+			var bot = new Player("BOT");
+			autoChess.AddPlayer(player);
+			autoChess.AddPlayer(bot);
+			
+			// PICK HERO MENU
+			// TODO
+			// 1. How to edit/remove item from user (if user want to change item that already playerHeroes)
+			// 2. Fix BarChart
+			autoChess.AddHero("Poisonous Worm", new HeroDetails(PieceTypes.Warlock, 600, 55, 0, 3));
+			autoChess.AddHero("Hell Knight", new HeroDetails(PieceTypes.Knight, 700, 75, 5, 1));
+			autoChess.AddHero("God of Thunder", new HeroDetails(PieceTypes.Mage, 950, 60, 0, 3));
+			autoChess.AddHero("Swordman", new HeroDetails(PieceTypes.Warrior, 600, 67.5, 5, 2));
+			autoChess.AddHero("Egersis Ranger", new HeroDetails(PieceTypes.Hunter, 450, 45, 5, 5));
+			autoChess.AddHero("Shadowcrawler", new HeroDetails(PieceTypes.Assassin, 550, 85, 5, 2));
+			autoChess.AddHero("Storm Shaman", new HeroDetails(PieceTypes.Shaman, 800, 47.5, 5, 4));
+			autoChess.AddHero("Warpwood Sage", new HeroDetails(PieceTypes.Druid, 650, 75, 5, 2));
+			autoChess.AddHero("Fallen Witcher", new HeroDetails(PieceTypes.Witcher, 750, 70, 5, 1));
+			autoChess.AddHero("Heaven Bomber ", new HeroDetails(PieceTypes.Mech, 500, 45, 5, 4));
+			autoChess.AddHero("Goddess of Light", new HeroDetails(PieceTypes.Priest, 400, 52.5, 0, 4));
+			autoChess.AddHero("Grand Herald", new HeroDetails(PieceTypes.Wizard, 600, 55, 0, 4));
+			var heroesDatabase = autoChess.HeroesDatabase;
+			var heroesOptions = new List<string>();
+			foreach(var key in heroesDatabase.Keys)
+			{
+				heroesOptions.Add(key.ToString());
+			}
+			List<Hero> playerHeroes = (List<Hero>)autoChess.GetPlayerPieces(player);
+			var heroOptionsStat = new List<IRenderable>();
 
-            while(playerHeroes.Count < 5 && roll > 0)
-            {
-                FigletTitle(font, "Pick Your Heroes");
-                autoChess.CurrentGamePhase = Phases.ChoosingPieace;
-                var optionsList = autoChess.GenerateRandomHeroList(in heroesOptions);
-                heroOptionsStat.Clear();
-                int barWidth = 35;
-                foreach(var hero in optionsList)
-                {
-                    var heroPanel = new Panel(
-                        new BarChart()
-                        .Width(barWidth)
-                        .AddItem("HP", heroesDatabase[hero].Hp, Color.Green)
-                        .AddItem("ATK", heroesDatabase[hero].Attack, Color.Red3)
-                        .AddItem("Armor", heroesDatabase[hero].Armor, Color.Blue)
-                        .AddItem("ATK Range", heroesDatabase[hero].AttackRange, Color.Red1)
-                    ).Header(new PanelHeader(hero));
-                    heroPanel.Padding = new Padding(0, 0, 0, 0);
-                    heroOptionsStat.Add(heroPanel);
-                }
-                AnsiConsole.Write(new Columns(heroOptionsStat));
-                var options = AnsiConsole.Prompt(
-                    new MultiSelectionPrompt<string>()
-                    .NotRequired()
-                    .PageSize(5)
-                    .AddChoices(optionsList)
-                    .InstructionsText(
-                        $"[grey](Press [blue]<space>[/] to select hero, [green]<enter>[/] to accept and re-roll)[/]"
-                        )
-                );
-                foreach(var pick in options)
-                {
-                    // Set player piece
-                    autoChess.AddPlayerPiece(player, new Hero(pick, heroesDatabase[pick]));
-                }
-                roll--;
-            }
-            // Keep first 5 item playerHeroes by user
-            if(playerHeroes.Count > 5)
-            {
-                playerHeroes = playerHeroes[0..5];
-            }
+			while(playerHeroes.Count < 5 && roll > 0)
+			{
+				FigletTitle(font, "Pick Your Heroes");
+				autoChess.CurrentGamePhase = Phases.ChoosingPieace;
+				var optionsList = autoChess.GenerateRandomHeroList(in heroesOptions);
+				heroOptionsStat.Clear();
+				int barWidth = 35;
+				foreach(var hero in optionsList)
+				{
+					var heroPanel = new Panel(
+						new BarChart()
+						.Width(barWidth)
+						.AddItem("HP", heroesDatabase[hero].Hp, Color.Green)
+						.AddItem("ATK", heroesDatabase[hero].Attack, Color.Red3)
+						.AddItem("Armor", heroesDatabase[hero].Armor, Color.Blue)
+						.AddItem("ATK Range", heroesDatabase[hero].AttackRange, Color.Red1)
+					).Header(new PanelHeader(hero));
+					heroPanel.Padding = new Padding(0, 0, 0, 0);
+					heroOptionsStat.Add(heroPanel);
+				}
+				AnsiConsole.Write(new Columns(heroOptionsStat));
+				var options = AnsiConsole.Prompt(
+					new MultiSelectionPrompt<string>()
+					.NotRequired()
+					.PageSize(5)
+					.AddChoices(optionsList)
+					.InstructionsText(
+						$"[grey](Press [blue]<space>[/] to select hero, [green]<enter>[/] to accept and re-roll)[/]"
+						)
+				);
+				foreach(var pick in options)
+				{
+					// Set player piece
+					autoChess.AddPlayerPiece(player, new Hero(pick, heroesDatabase[pick]));
+				}
+				roll--;
+			}
+			// Keep first 5 item playerHeroes by user
+			if(playerHeroes.Count > 5)
+			{
+				playerHeroes = playerHeroes[0..5];
+			}
 
-            // BOT 
-            // Bot pick piece
-            foreach(var botPick in autoChess.GenerateRandomHeroList(in heroesOptions))
-            {
-                autoChess.AddPlayerPiece(bot, new Hero(botPick, heroesDatabase[botPick]));
-            }
-            
-            // SET HERO POSITION MENU
-            autoChess.CurrentGamePhase = Phases.PlaceThePiece;
-            // Loop until all player's piece on the board
-            while(!autoChess.IsFinishedPutAllPieces(player))
-            {
-                FigletTitle(font, "Place Your Heroes");
-                AnsiConsole.Write(new Rule("[red]Player Hero's Position[/]"));
-                var playerPieces = autoChess.GetPlayerData(player).PlayerPieces;
-                // TODO
-                // 1. Change preview layout using board
-                foreach(var piece in playerPieces)
-                {
-                    AnsiConsole.WriteLine($"{piece} | X:{piece.HeroPosition.X} Y:{piece.HeroPosition.Y}");
-                }
-                AnsiConsole.Write(new Rule("[red]Set Hero's Position[/]"));
-                var playerPiece = AnsiConsole.Prompt(
-                    new SelectionPrompt<Hero>()
-                    .PageSize(5)
-                    .AddChoices(
-                        playerPieces
-                    )
-                );
-                // Loop until piece placement success
-                bool success = false;
-                while(!success)
-                {
-                    var pieceX = AnsiConsole.Prompt(
-                        new TextPrompt<int>($"{playerPiece}'s [green]X[/] position?")
-                        .PromptStyle("green")
-                        .ValidationErrorMessage("[red]That's not a valid coordinate[/]")
-                        .Validate(coordinate =>
-                            {
-                                return coordinate switch
-                                {
-                                    < 0 => ValidationResult.Error("[red]The coordinate must be positive[/]"),
-                                    >= boardSize => ValidationResult.Error($"[red]The coordinate can't exceed the player's area ({boardSize})[/]"),
-                                    _ => ValidationResult.Success(),
-                                };
-                            }
-                        )
-                    );
+			// BOT 
+			// Bot pick piece
+			foreach(var botPick in autoChess.GenerateRandomHeroList(in heroesOptions))
+			{
+				autoChess.AddPlayerPiece(bot, new Hero(botPick, heroesDatabase[botPick]));
+			}
+			
+			// SET HERO POSITION MENU
+			autoChess.CurrentGamePhase = Phases.PlaceThePiece;
+			// Loop until all player's piece on the board
+			while(!autoChess.IsFinishedPutAllPieces(player))
+			{
+				FigletTitle(font, "Place Your Heroes");
+				AnsiConsole.Write(new Rule("[red]Player Hero's Position[/]"));
+				var playerPieces = autoChess.GetPlayerData(player).PlayerPieces;
+				// TODO
+				// 1. Change preview layout using board
+				foreach(var piece in playerPieces)
+				{
+					AnsiConsole.WriteLine($"{piece} | X:{piece.HeroPosition.X} Y:{piece.HeroPosition.Y}");
+				}
+				AnsiConsole.Write(new Rule("[red]Set Hero's Position[/]"));
+				var playerPiece = AnsiConsole.Prompt(
+					new SelectionPrompt<Hero>()
+					.PageSize(5)
+					.AddChoices(
+						playerPieces
+					)
+				);
+				// Loop until piece placement success
+				bool success = false;
+				while(!success)
+				{
+					var pieceX = AnsiConsole.Prompt(
+						new TextPrompt<int>($"{playerPiece}'s [green]X[/] position?")
+						.PromptStyle("green")
+						.ValidationErrorMessage("[red]That's not a valid coordinate[/]")
+						.Validate(coordinate =>
+							{
+								return coordinate switch
+								{
+									< 0 => ValidationResult.Error("[red]The coordinate must be positive[/]"),
+									>= boardSize => ValidationResult.Error($"[red]The coordinate can't exceed the player's area ({boardSize})[/]"),
+									_ => ValidationResult.Success(),
+								};
+							}
+						)
+					);
 
-                    var pieceY = AnsiConsole.Prompt(
-                        new TextPrompt<int>($"{playerPiece}'s [green]Y[/] position?")
-                        .PromptStyle("green")
-                        .ValidationErrorMessage("[red]That's not a valid coordinate[/]")
-                        .Validate(coordinate =>
-                            {
-                                return coordinate switch
-                                {
-                                    < 0 => ValidationResult.Error("[red]The coordinate must be positive[/]"),
-                                    >= boardSize / 2 => ValidationResult.Error($"[red]The coordinate can't exceed the player's area ({boardSize / 2})[/]"),
-                                    _ => ValidationResult.Success(),
-                                };
-                            }
-                        )
-                    );
-                    success = autoChess.PutPlayerPiece(playerPiece, new Position(pieceX, pieceY));
-                    if(!success)
-                    {
-                        AnsiConsole.Markup("[red]You can't put another hero in the same coordinate[/]\n");
-                    }
-                }
-            }
+					var pieceY = AnsiConsole.Prompt(
+						new TextPrompt<int>($"{playerPiece}'s [green]Y[/] position?")
+						.PromptStyle("green")
+						.ValidationErrorMessage("[red]That's not a valid coordinate[/]")
+						.Validate(coordinate =>
+							{
+								return coordinate switch
+								{
+									< 0 => ValidationResult.Error("[red]The coordinate must be positive[/]"),
+									>= boardSize / 2 => ValidationResult.Error($"[red]The coordinate can't exceed the player's area ({boardSize / 2})[/]"),
+									_ => ValidationResult.Success(),
+								};
+							}
+						)
+					);
+					success = autoChess.PutPlayerPiece(playerPiece, new Position(pieceX, pieceY));
+					if(!success)
+					{
+						AnsiConsole.Markup("[red]You can't put another hero in the same coordinate[/]\n");
+					}
+				}
+			}
 
-            // BOT
-            // Bot put piece
-            foreach(var piece in autoChess.GetPlayerData(bot).PlayerPieces)
-            {
-                bool success = false;
-                while(!success)
-                {
-                    int x = new Random().Next(0, boardSize);
-                    int y = new Random().Next(4, boardSize);
-                    success = autoChess.PutPlayerPiece(piece, new Position(x, y));
-                }
-            }
+			// BOT
+			// Bot put piece
+			foreach(var piece in autoChess.GetPlayerData(bot).PlayerPieces)
+			{
+				bool success = false;
+				while(!success)
+				{
+					int x = new Random().Next(0, boardSize);
+					int y = new Random().Next(4, boardSize);
+					success = autoChess.PutPlayerPiece(piece, new Position(x, y));
+				}
+			}
 
-            // PREVIEW MENU
-            // TODO
-            // 1. Change preview layout using board
-            FigletTitle(font, "Preview");
-            // Display player piece's position
-            AnsiConsole.Write(new Rule("[red]Player Hero's Position[/]"));
-            foreach(var piece in autoChess.GetPlayerData(player).PlayerPieces)
-            {
-                AnsiConsole.WriteLine($"{piece} | X:{piece.HeroPosition.X} Y:{piece.HeroPosition.Y}");
-            }
-            
-            // Display BOT piece's position
-            AnsiConsole.Write(new Rule("[red]Bot Hero's Position[/]"));
-            foreach(var piece in autoChess.GetPlayerData(bot).PlayerPieces)
-            {
-                AnsiConsole.WriteLine($"{piece} | X:{piece.HeroPosition.X} Y:{piece.HeroPosition.Y}");
-            }
-        }
-    }
+			// PREVIEW MENU
+			// TODO
+			// 1. Change preview layout using board
+			FigletTitle(font, "Preview");
+			// Display player piece's position
+			AnsiConsole.Write(new Rule("[red]Player Hero's Position[/]"));
+			foreach(var piece in autoChess.GetPlayerData(player).PlayerPieces)
+			{
+				AnsiConsole.WriteLine($"{piece} | X:{piece.HeroPosition.X} Y:{piece.HeroPosition.Y}");
+			}
+			
+			// Display BOT piece's position
+			AnsiConsole.Write(new Rule("[red]Bot Hero's Position[/]"));
+			foreach(var piece in autoChess.GetPlayerData(bot).PlayerPieces)
+			{
+				AnsiConsole.WriteLine($"{piece} | X:{piece.HeroPosition.X} Y:{piece.HeroPosition.Y}");
+			}
+		}
+	}
 }
