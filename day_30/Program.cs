6212266ea7c31cce38cb4040e32e38b63cbe9382
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2024-01-17 17:04:03 +0700

    Conditional Compilation

---
+++ day_30/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+	static void Main()
+	{
+		// Conditional Compilation
+		#if COBA
+		#region COBA
+		Console.WriteLine("Awalnya sih coba-coba ya... eh keterusan");
+		#endregion
+		#elif DEBUG
+		#region DEBUG
+		Console.WriteLine("Daebak");
+		#endregion
+		#elif RELEASE
+		#region RELEASE
+		Console.WriteLine("Please release me...");
+		#endregion
+		#endif
+	}
+}
