b98823dd3d270cd4b39f560312874f5393984835
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2024-01-23 09:49:54 +0700

    Read and write file

---
+++ day_36/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+	static void Main()
+	{
+		// StreamWriter
+		string path = @"output.txt";
+		FileStreamOptions options = new()
+		{
+			Mode = FileMode.OpenOrCreate,
+			Access = FileAccess.Write,
+			Share = FileShare.None
+		};
+		// using(FileStream fs = new(path, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+		// {
+		// }
+		using(StreamWriter writer = new(path, options))
+		{
+			writer.WriteLine("Annyeonghaseyo");
+			Console.ReadKey();
+		}
+		
+		// StreamReader
+		// using(FileStream fs = new(path, FileMode.OpenOrCreate, FileAccess.Read))
+		// {
+		// }
+		using(StreamReader reader = new(path))
+		{
+			Console.WriteLine(reader.ReadToEnd());
+		}
+	}
+}
