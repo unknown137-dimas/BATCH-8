5f57768845226f4c56e049856f2026965223b0f3
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2024-01-24 20:51:02 +0700

    Add more side option

--- AutoChess/Enums/Sides.cs
+++ AutoChess/Enums/Sides.cs
@@ -2,5 +2,15 @@ public enum Sides
 {
     Red,
     Blue,
+    Yellow,
+    Fuchsia,
+    Aqua,
+    Lime,
+    Grey,
+    Purple_2,
+    Orange3,
+    Gold1,
+    LightCoral,
+    HotPink,
     Unknown,
 }
