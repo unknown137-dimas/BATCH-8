e5bda2ccfab6a863c8d8452e158a029dce44ea1d
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2024-01-26 08:00:15 +0700

    Update method that count win point using linq

--- AutoChess/Classes/PlayerData.cs
+++ AutoChess/Classes/PlayerData.cs
@@ -1,40 +1,29 @@
 public class PlayerData
 {
-    public List<IPiece> PlayerPieces = new();
-    public bool Winner;
-    public int Hp;
-    public List<bool> Win = new();
+	public List<IPiece> PlayerPieces = new();
+	public bool Winner;
+	public int Hp;
+	public List<bool> Win = new();
 
-    public Sides PlayerSide;
+	public Sides PlayerSide;
 
-    public PlayerData(int hp, Sides playerSide)
-    {
-        Hp = hp;
-        PlayerSide = playerSide;
-    }
+	public PlayerData(int hp, Sides playerSide)
+	{
+		Hp = hp;
+		PlayerSide = playerSide;
+	}
 
-    public IPiece GetPieceById(Guid heroId)
-    {
-        var result = PlayerPieces.FirstOrDefault(piece => piece.PieceId == heroId);
-        return result != null ? result : throw new KeyNotFoundException();
-    }
+	public IPiece GetPieceById(Guid heroId)
+	{
+		var result = PlayerPieces.FirstOrDefault(piece => piece.PieceId == heroId);
+		return result != null ? result : throw new KeyNotFoundException();
+	}
 
-    public bool TryGetPieceById(Guid heroId, out IPiece? pieceResult)
-    {
-        pieceResult = PlayerPieces.FirstOrDefault(piece => piece.PieceId == heroId);
-        return pieceResult != null;
-    }
+	public bool TryGetPieceById(Guid heroId, out IPiece? pieceResult)
+	{
+		pieceResult = PlayerPieces.FirstOrDefault(piece => piece.PieceId == heroId);
+		return pieceResult != null;
+	}
 
-
-    public int GetWinPoint()
-    {
-        int winPoint = 0;
-        Win.ForEach(win => {
-            if(win)
-            {
-                winPoint++;
-            }
-        });
-        return winPoint;
-    }
+	public int GetWinPoint() => Win.Where(win => win).Count();
 }
