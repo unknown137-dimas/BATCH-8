7f9eb94a326c6e3893f36632f6d6aef0ba37c998
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2024-01-29 14:27:25 +0700

    Logger example

---
+++ day_42_logger/Program.cs
@@ -0,0 +1,15 @@
+﻿using NLog;
+
+class Program
+{
+	private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+	
+	static void Main()
+	{
+		logger.Info("AAAAAAAAAA");
+		logger.Info("Program started.");
+		logger.Warn("WARN MESSAGE");
+		logger.Error("ERROR MESSAGE");
+		logger.Fatal("FATAL MESSAGE");
+	}
+}
