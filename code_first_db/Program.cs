a6bcc9fd7d4ba6e225b09e8b5535f34c3f0193ba
unknown137-dimas <dimasfitriokurniaw4n@gmail.com>
2024-01-31 10:29:40 +0700

    Add category data entry

--- code_first_db/Program.cs
+++ code_first_db/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using code_first_db;
+
+class Program
+{
+	static async Task Main()
+	{
+		using(Southwind sw = new())
+		{
+			List<Category> newCategories = [
+				new Category()
+				{
+					CategoryName = "Electronics",
+					Description = "Electronics and Parts"
+				},
+				new Category()
+				{
+					CategoryName = "Automotive",
+					Description = "Automotives Parts"
+				},
+				new Category()
+				{
+					CategoryName = "Seafood",
+					Description = "Fish, Shrimp, and Sea Shell"
+				},
+				new Category()
+				{
+					CategoryName = "Beverage",
+					Description = "Drinks and Beverages"
+				}
+			];
+			
+			foreach(var newCategory in newCategories)
+			{
+				var result = sw.Categories.FirstOrDefault(c => c.CategoryName.Contains(newCategory.CategoryName));
+				if(result is null)
+				{
+					await sw.Categories.AddAsync(newCategory);
+				}
+			}
+			await sw.SaveChangesAsync();
+		}
+	}
+}
